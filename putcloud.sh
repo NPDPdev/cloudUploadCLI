f9c1f713ce5f38b91f799609da705aee49a522fa
Ivanna Platthy <69126161+NPDPdev@users.noreply.github.com>
2024-04-23 23:15:51 +0000

    AWS CLI installer implemented.

--- putcloud.sh
+++ putcloud.sh
@@ -10,28 +10,46 @@ azSetup() {
     echo 'Login Successful'
 }
 
+#Setup AWS Authentication
+
+awsSetup() { 
+    # Install aws cli
+    pushd "/tmp"
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+
+    rm "awscliv2.zip"
+    rm -r "./aws"
+    popd
+
+
+}
 
 
 
 
-
-if[$1='setup'];
+if [ "$1" == "setup" ]
 then
-  read -p 'Authenticate with [1]Azure or [2]AWS: ' hostSelection
+  read -p 'Authenticate with [Azure] or [AWS]: ' hostSelection
   
   case $hostSelection in
     Azure) 
         echo "Chose Azure";;
     AWS) 
-        echo "Chose AWS";;
+        echo "Chose AWS"
+        awsSetup;;
     *) 
         echo 'You failed to input "Azure" or "AWS"';;
   esac
   
-else
+#else
   #establish other functions like put, list, etc
 fi
   
 
 
 #Setup AWS Authentication
+
+##check if user is root
+##check if aws and azure is already installed
