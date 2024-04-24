90dca2cfddf1aadb6f30ee9dfd0b2959db842b70
Ivanna Platthy <69126161+NPDPdev@users.noreply.github.com>
2024-04-24 02:55:54 +0000

    Prepped install function and fleshed out setups

--- install-putCloud.sh
+++ install-putCloud.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
 
-#Setup Azure Authentication
+# if [ -d "$/usr/local/putcloud" ]; then
+#   echo "putCloud seems to be installed already, exiting."
+#   echo "Run "install-putcloud.sh uninstall" to remove putCloud and its files."
+#   exit 1 #actually make uninstall option
+# fi
 
+# mkdir "/usr/local/putcloud"
+# pushd "/usr/local/putcloud"
+
+
+# #Create config file.
+
+# cat > putcloud.conf << EOF
+# ConnectionType none
+# StorageContainer none
+# EOF
+
+
+# popd
+
+
+
+
+
+#cat >/usr/local/bin/putcloud <<EOF
+
+
+
+
+
+#Setup Azure Authentication
 azSetup() { 
-    #Install az cli
-    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-    #Login
+
+    #Install Azure cli if not currently installed
+    if ! [ -x "$(command -v az)" ]; then
+
+      curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+    else
+
+      echo "Azure CLI is already installed, skipping install!"
+
+    fi
+
+    #Login and auth verification
     az login --use-device-code
-    echo 'Login Successful'
+    az account show > /dev/null
+
+    if [[ $? -eq 0 ]]; then
+      echo "Successfully authenticated with Azure! Setup complete."
+    else
+      echo "There may have been an error authenticating with Azure."
+      echo "Please try running setup again."
+      exit 1
+    fi
+    
+
+
+    #section to specify default azure blob
+
 }
+
+
+
 
 
 #Setup AWS Authentication
@@ -36,17 +91,19 @@ awsSetup() {
     fi
 
 
-    #Authentication procedure
+
+    #Login and auth verification
     local awsAuthType
     read -p 'Would you like to authenticate with [SSO] or Access Key [AK]: ' awsAuthType
   
     case $awsAuthType in
-      SSO) 
+      SSO | sso) 
         aws configure sso;;
-      AK) 
+      AK | ak) 
         aws configure;;
       *) 
-        echo 'You failed to input "SSO" or "AK", quitting.';;
+        echo 'You failed to input "SSO" or "AK", quitting.'
+        exit 1;;
     esac
 
     aws sts get-caller-identity > /dev/null
@@ -56,8 +113,15 @@ awsSetup() {
     else
       echo "There may have been an error authenticating with AWS."
       echo "Please try running setup again."
+      exit 1
     fi
     
+
+
+  #section to specify default aws bucket
+
+
+
 }
 
 
@@ -66,30 +130,57 @@ awsSetup() {
 
 
 
-if [ "$1" == "setup" ] #make case statement
-then
-  read -p 'Authenticate with [Azure] or [AWS]: ' hostSelection
-  
-  case $hostSelection in
-    Azure) 
-        echo "Chose Azure";;
-    AWS) 
-        echo "Chose AWS"
-        awsSetup;;
-    *) 
-        echo 'You failed to input "Azure" or "AWS", quitting.';;
+
+
+
+
+
+
+
+#colon in front of hs disables error reporting but may lead to bad stuff!!
+while getopts ":hs" clArg; do
+  case "${clArg}" in
+    h)
+      #help information
+      echo "tempHelp"
+
+      exit 0
+      ;;
+    
+    s)
+
+      #Authentication and installation of AWS CLI or Azure CLI
+      read -p 'Authenticate with [Azure] or [AWS]: ' hostSelection
+      case $hostSelection in
+        Azure | azure) 
+          azSetup;;
+        AWS | aws) 
+          awsSetup;;
+        *) 
+          echo 'You failed to input "Azure" or "AWS", quitting.';;
+      esac
+      ;;
+
+
+    *)
+      echo "Invalid parameters. Run 'putcloud -h' for help."
+      exit 1
+      ;;
   esac
-  
-#else
-  #establish other functions like put, list, etc
-fi
-  
+done
 
 
-#Setup AWS Authentication
+
+
+
+#EOF
+
+
+#AWS
 #assume they'll use aws cli for most management stuff, don't need to reimpliment anything more than basic stuff
 #upload, upload status failed or succeded (report errors), allow multiple uploads, ow/skip/rename, specify remote directory
 
 
 ##check if user is root
 ##check if aws and azure is already installed
+#make uninstall flag
