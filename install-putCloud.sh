1925444a7dc903af7f358be0fc4a37e5c889ab4a
Ivanna Platthy <69126161+NPDPdev@users.noreply.github.com>
2024-04-24 22:13:42 +0000

    Changed arg handling. Setup functions properly.

--- install-putCloud.sh
+++ install-putCloud.sh
@@ -6,15 +6,15 @@
 #   exit 1 #actually make uninstall option
 # fi
 
-# mkdir "/usr/local/putcloud"
+# sudo mkdir "/usr/local/putcloud"
 # pushd "/usr/local/putcloud"
 
 
 # #Create config file.
 
-# cat > putcloud.conf << EOF
-# ConnectionType none
-# StorageContainer none
+# sudo cat > putcloud.conf << EOF
+# ConnectionType=none
+# StorageContainer=none
 # EOF
 
 
@@ -30,7 +30,7 @@
 
 
 
-#Setup Azure Authentication
+#Setup Azure Authentication // Need to modify apropriate configs
 azSetup() { 
 
     #Install Azure cli if not currently installed
@@ -50,6 +50,7 @@ azSetup() {
 
     if [[ $? -eq 0 ]]; then
       echo "Successfully authenticated with Azure! Setup complete."
+      echo
     else
       echo "There may have been an error authenticating with Azure."
       echo "Please try running setup again."
@@ -59,6 +60,7 @@ azSetup() {
 
 
     #section to specify default azure blob
+    echo "azure blob selection NYI"
 
 }
 
@@ -66,7 +68,7 @@ azSetup() {
 
 
 
-#Setup AWS Authentication
+#Setup AWS Authentication // Need to modify apropriate configs
 
 awsSetup() { 
     #Checks if aws cli is NOT installed
@@ -94,7 +96,10 @@ awsSetup() {
 
     #Login and auth verification
     local awsAuthType
+    local bucketDefault
+
     read -p 'Would you like to authenticate with [SSO] or Access Key [AK]: ' awsAuthType
+    echo
   
     case $awsAuthType in
       SSO | sso) 
@@ -109,18 +114,77 @@ awsSetup() {
     aws sts get-caller-identity > /dev/null
 
     if [[ $? -eq 0 ]]; then
-      echo "Successfully authenticated with AWS! Setup complete."
+      echo
+      echo "Successfully authenticated with AWS!"
+      echo
     else
       echo "There may have been an error authenticating with AWS."
       echo "Please try running setup again."
       exit 1
     fi
     
+    read -p 'Specify the bucket that you will be uploading to: ' bucketDefault
+    bbSelect $bucketDefault > /dev/null
+
+    echo "Default bucket is now:" $bucketDefault
+    echo
+    echo "Setup complete!"
 
 
-  #section to specify default aws bucket
+}
 
 
+
+#Change blob/bucket in config
+bbSelect(){
+  local bbNew
+  local tempSed
+  
+  #Load Config
+  . /usr/local/putcloud/putcloud.conf
+  
+  if [[ $# = 0 ]]; then
+    echo "Current value is: $StorageContainer"
+    read -p 'Enter new location: ' bbNew
+  else
+    bbNew="$1"
+    
+  fi
+
+  echo 'Updated value from "'$StorageContainer'" to "'$bbNew'".'  #Also make it differentiate between "bucket" and "blob" instead of just "value"
+  tempSed="s/StorageContainer=.*/StorageContainer=$bbNew/g" #part of the "this" in question
+
+  sudo sed -i $tempSed /usr/local/putcloud/putcloud.conf #oh this DEFINITELY needs sanitized
+}
+
+
+
+#Uploads files to preconfigured blob/bucket
+fileSend(){
+
+  #Load Config
+  . /usr/local/putcloud/putcloud.conf
+
+  case "$ConnectionType" in
+    AWS)
+      aws s3 cp ./$1 s3://$StorageContainer/$2/
+
+      if [[ $? -eq 0 ]]; then
+        echo "Upload successful."
+      else
+        echo "Upload failed."
+      fi
+      
+    ;;
+    Azure)
+      echo "using Azure to upload (NYI)"
+    ;;
+    *)
+      echo "Your connection type is unset or misconfigured."
+      echo "Please run putcloud with the -s flag to access setup."
+      ;;
+  esac
+  
 
 }
 
@@ -132,41 +196,80 @@ awsSetup() {
 
 
 
+while true; do
+    case $1 in
+
+      -h)
+        #help information
+        echo "tempHelp"
+
+        exit 0
+        ;;
 
 
+      -s)
+
+        #Authentication and installation of AWS CLI or Azure CLI
+        read -p 'Authenticate with [Azure] or [AWS]: ' hostSelection
+        case $hostSelection in
+          Azure | azure) 
+            azSetup;;
+          AWS | aws) 
+            awsSetup;;
+          *) 
+            echo 'You failed to input "Azure" or "AWS", quitting.';;
+        esac
+
+        exit 0
+        ;;
 
 
+      -b)
 
-#colon in front of hs disables error reporting but may lead to bad stuff!!
-while getopts ":hs" clArg; do
-  case "${clArg}" in
-    h)
-      #help information
-      echo "tempHelp"
+        #Blob/Bucket selection
 
-      exit 0
-      ;;
-    
-    s)
+        if [ $# = 2 ]; then
+          bbSelect $2
+        else
+          bbSelect
+        fi
 
-      #Authentication and installation of AWS CLI or Azure CLI
-      read -p 'Authenticate with [Azure] or [AWS]: ' hostSelection
-      case $hostSelection in
-        Azure | azure) 
-          azSetup;;
-        AWS | aws) 
-          awsSetup;;
-        *) 
-          echo 'You failed to input "Azure" or "AWS", quitting.';;
-      esac
-      ;;
+        exit 0
+        ;;
+
+      -p) 
+        #Shifts between arguments and assigns variables to give to fileSend() function
+        #Doing this for now because there is potential to include multiple files
+        if [ $# -gt 2 ]; then
+
+          shift
+          fileToSend="$1"
+          shift
+          sendDestination="$1"
+
+          #Sends file to cloud
+          fileSend $fileToSend $sendDestination
+          
 
 
-    *)
-      echo "Invalid parameters. Run 'putcloud -h' for help."
-      exit 1
-      ;;
-  esac
+        else
+
+          echo "Incomplete command."
+          echo 'Run "putcloud -h" for help.'
+          exit 2
+        fi
+ 
+        exit 0
+        ;;
+          
+      *)
+        if [ $# -gt 1 ]; then
+          echo $0': Invalid parameter: "'$1'"' >&2
+        fi
+        
+        echo 'Run "putcloud -h" for help.'
+        exit 2;;
+    esac
 done
 
 
@@ -182,5 +285,4 @@ done
 
 
 ##check if user is root
-##check if aws and azure is already installed
 #make uninstall flag
